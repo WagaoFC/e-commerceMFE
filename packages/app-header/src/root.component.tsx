844f6f9235023b33c73ff5b934e3be302c63b56d
wagner.costa <wagner.costa@bit.com.br>
2023-08-31 15:19:05 -0300

    :sparkles: feat: sharing state

--- packages/app-header/src/root.component.tsx
+++ packages/app-header/src/root.component.tsx
@@ -1,10 +1,14 @@
 // @ts-ignore
 import { Navigate } from "@shoppe/util-ui"
+// @ts-ignore
+import { useStore } from "@shoppe/state-ui"
 
 export default function Root(props) {
+  const store = useStore
   return (
     <section>
       <h1>{props.name} is mounted!</h1>
+      <h3>{store.counter}</h3>
       <nav>
         <Navigate path='/account'>Account</Navigate>
         <Navigate path='/cart'>Cart</Navigate>
